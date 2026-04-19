8811b02d52a23e87d72d733ab6666ac4c51db9dd
61538699-cpu <61538699@continental.edu.pe>
2026-04-19 11:49:33 -0500

    Update api-test.js

---
+++ api-test.js
@@ -0,0 +1,10 @@
+const claveApi = 'd8c52f83e92d40cb88921331261904';
+const idioma = 'es';
+const ciudad = 'Huancayo';
+
+const apiClimaActual = `https://api.weatherapi.com/v1/current.json?q=${ciudad}&lang=${idioma}&key=${claveApi}`;
+
+const response = await fetch(apiClimaActual)
+let data = await response.json();
+
+console.log(data);
